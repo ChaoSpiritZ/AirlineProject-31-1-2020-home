260e7d4a8ab4f537829ddc69231df11f209f0de0
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2020-02-06 19:09:32 +0200

    Add files via upload

---
+++ AirlineProject-6-2-2020-class/AirlineProject/FlyingCenterSystem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineProject
+{
+    public class FlyingCenterSystem
+    {
+        private static FlyingCenterSystem INSTANCE;
+
+        private FlyingCenterSystem()
+        {
+
+        }
+
+        public static FlyingCenterSystem GetInstance()
+        {
+            if (INSTANCE == null)
+            {
+                INSTANCE = new FlyingCenterSystem();
+            }
+            return INSTANCE;
+        }
+
+        public FacadeBase Login(string username, string pwd, out ILoginToken loginToken)
+        {
+            LoginService lS = new LoginService();
+            if (lS.TryAdminLogin(username, pwd, out LoginToken<Administrator> adminToken))
+            {
+                loginToken = adminToken;
+                return new LoggedInAdministratorFacade();
+            }
+            if(lS.TryAirlineLogin(username, pwd, out LoginToken<AirlineCompany> airlineToken))
+            {
+                loginToken = airlineToken;
+                return new LoggedInAirlineFacade();
+            }
+            if(lS.TryCustomerLogin(username, pwd, out LoginToken<Customer> customerToken))
+            {
+                loginToken = customerToken;
+                return new LoggedInCustomerFacade();
+            }
+            //if username null => return anonymous facade
+            loginToken = null;
+            return null;
+        }
+        
+    }
+}
